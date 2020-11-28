abdad08bf8725ee10d5fa84f2b5c6610c726ca6b
Barrie Wu <wu.barrie@gmail.com>
2020-11-28 17:52:05 -0500

    Initial Commit :rocket: Files set up :wrench:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>fashion inspo</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam laboriosam dolorum corporis debitis assumenda dicta soluta iure, adipisci, laudantium dolores impedit praesentium? Dolore cumque obcaecati quo corporis ex delectus voluptatum consequatur perspiciatis in minima eveniet labore repudiandae, eum voluptas nisi nulla facere nihil libero officia! Sint similique blanditiis suscipit, cupiditate iste eveniet id obcaecati? Exercitationem dolorem at nulla ab laudantium asperiores, eveniet veritatis commodi excepturi voluptatem nihil consequuntur distinctio. Alias sequi tempora quis voluptas aliquam repellendus, illum corporis beatae? Consequuntur dolor deleniti odit ex ab minus fugiat mollitia quam autem veritatis, culpa neque temporibus error iure tempora. Fugit, repellendus optio?</p>
+          <button className="startButton">get started</button>
+        </header>
+        <div className="gallery"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
