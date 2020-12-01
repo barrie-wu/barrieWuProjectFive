731a90fcf7eddcd855a0393086a4c9d6d99b7539
Barrie Wu <wu.barrie@gmail.com>
2020-11-30 21:57:42 -0500

    Functionality to switch between main gallery and user gallery

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import MainGallery from './MainGallery.js';
+import UserGallery from './UserGallery.js'
 import { Link } from 'react-scroll';
 import Footer from './Footer.js'
 import './App.css';
@@ -10,14 +11,24 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      looks: false
+      looks: false,
+      userGallery: false
     }
   }
-
-  // SHOW GALLERY CLICK HANDLER
+  
+  // show main gallery click handler
   handleShowLooks = () => {
     this.setState({
       looks: true
+    })
+  }
+
+  // show user gallery and hide main gallery
+  handleToggleGallery = () => {
+    this.setState({
+      // ! toggle true or false
+      userGallery: !this.state.userGallery,
+      looks: !this.state.looks
     })
   }
 
@@ -25,7 +36,8 @@ class App extends Component {
     return (
       <div className="App">
         <header>
-          <FaRegAddressCard className="galleryToggleButton"/>
+          {/* click handler on profile icon to switch between main and user gallery */}
+          <FaRegAddressCard className="galleryToggleButton" onClick={this.handleToggleGallery}/>
           <div className="insideWrapper headerContents">
             <h1>fashion inspo.</h1>
             <section className="aboutUs">
@@ -33,7 +45,9 @@ class App extends Component {
               <p className="aboutUsText">Welcome to my fashion inspiration gallery. Here you will find a carefully curated collection of fashion photography that will help with inspiring your next outfit. Simply click on the heart in the top-right corner of each image to save it to your own collection. To switch between your collection and the main collection, use the icon at the top-right of the main window. Press the "get looks" button to load the main gallery.</p>
             </section>
             <div className="startButtonContainer">
+              {/* scroll to main section */}
               <Link activeClass="active" to="gallery" spy={true} smooth={true}>
+                {/* click handler to show main gallery */}
                 <button onClick={this.handleShowLooks} className="startButton" tabIndex="0">get looks</button>
               </Link>
             </div>
@@ -41,7 +55,10 @@ class App extends Component {
         </header>
         <main id="gallery">
           <div className="insideWrapper">
+              {/* conditional if this.state.looks is true, show MainGallery */}
               {this.state.looks ? <MainGallery /> : null}
+              {/* conditinoal if this.state.userGallery is true show UserGallery and set MainGallery to false, refer to above conditional */}
+              {this.state.userGallery ? <UserGallery /> : null}
           </div>
         </main>
         <Footer />
